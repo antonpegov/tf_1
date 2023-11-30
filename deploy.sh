c024831124cfdad004e9a61df9186c02a6d78126
Anton Pegov <antonpegov@gmail.com>
2023-11-30 09:38:38 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+# Turn on Error Handling
 set -e
 ECR_URL="665162665527.dkr.ecr.eu-west-1.amazonaws.com/adm022"
 IMAGE="${ECR_URL}:$(cd uploadhub && git rev-parse --short HEAD)"
 
 echo $IMAGE
+
 docker build -t $IMAGE -f uploadhub/Dockerfile uploadhub/
 
-# login to our ECR
+echo ___________________Login to our ECR...
 
-eval $(aws ecr get-login --no-include-email --region eu-west-1)
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin $ECR_URL
+
+echo ___________________Pushing image to ECR...
+
 docker push $IMAGE
+
+echo ___________________Terraforming...
+
 export TF_VAR_image=$IMAGE
+
 terraform apply -replace="aws_instance.this" -auto-approve 
